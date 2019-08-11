90fcf0c6d594922bf39ffa4d5496548ad6517644
wdd <2759457402@qq.com>
2019-08-11 18:04:25 +0800

    第一次提交

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ConsoleApp2
+{
+    class Class1
+    {
+        public static string a;
+
+        //public string a;
+        public static void Main()
+        {
+            a = Environment.CurrentDirectory;
+            Console.WriteLine(Path.Combine(a, "JRE"));
+
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = " xiao mi  ";
+            string[] lines = result.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine(lines.Length);
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+        }
+    }
+}
